6bd63c04916dc2fb7ff9cfebd237f8c97799abe4
Sairaj Mishra <rickysairaj@gmail.com>
2021-08-03 23:22:13 +0530

    Added RNCamera and Image Picker

--- components/main/Scan.js
+++ components/main/Scan.js
@@ -1,13 +1,134 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-export class Scan extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Hello</Text>
-            </View>
-        )
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import {launchImageLibrary} from 'react-native-image-picker';
+
+
+export default class Scan extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      cameraType: 'back',
+      mirrorMode: false,
+      imageUri: null,
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          captureAudio={false}
+          style={styles.preview}
+          type={this.state.cameraType}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          
+          // onGoogleVisionBarcodesDetected={({ barcodes }) => {
+          //   console.log(barcodes);
+          // }}
+        />
+        <View style={{flexDirection:'row',justifyContent:'center'}}>
+        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.changeCamera.bind(this)} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SWAP </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.capture} onPress={() => this.chooseFile('photo')}>
+          <Text style={styles.textStyle}>Choose Image</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=> this.props.navigation.navigate('save',{image:this.state.imageUri})} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> Save </Text>
+        </TouchableOpacity>
+        </View>
+        {this.state.imageUri && <Image source={{uri:this.state.imageUri}} style={{flex:1}}/>}
+        
+      </View>
+    );
+  }
+  changeCamera(){
+    if(this.state.cameraType==='back'){
+      this.setState({
+        cameraType: 'front'
+      })
     }
+    else{
+      this.setState({
+        cameraType: 'back'
+      })
+    }
+  }
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      this.setState({
+        imageUri:data.uri
+      })
+      console.log(data.uri);
+    }
+  };
+  chooseFile = async (type) => {
+    let options = {
+      mediaType: type,
+      quality: 1,
+    };
+    launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        alert('Image not picked');
+        return;
+      } else if (response.errorCode == 'camera_unavailable') {
+        alert('Camera not available on device');
+        return;
+      } else if (response.errorCode == 'permission') {
+        alert('Permission not satisfied');
+        return;
+      } else if (response.errorCode == 'others') {
+        alert(response.errorMessage);
+        return;
+      }
+      console.log('base64 -> ', response.assets[0].base64);
+      console.log('uri -> ', response.assets[0].uri);
+      console.log('width -> ', response.assets[0].width);
+      console.log('height -> ', response.assets[0].height);
+      console.log('fileSize -> ', response.assets[0].fileSize);
+      console.log('type -> ', response.assets[0].type);
+      console.log('fileName -> ', response.assets[0].fileName);
+      this.setState({
+        imageUri:response.assets[0].uri
+      })
+    });
+  };
 }
 
-export default Scan
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    
+  },
+});
