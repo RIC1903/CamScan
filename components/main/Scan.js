99045415dd6746883380433f0a0de6103a6b7a1f
Sairaj Mishra <rickysairaj@gmail.com>
2021-08-04 21:01:56 +0530

    Replaced RNCamera with RN Image Crop Picker module

--- components/main/Scan.js
+++ components/main/Scan.js
@@ -1,122 +1,95 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, FlatList } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import {launchImageLibrary} from 'react-native-image-picker';
-
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default class Scan extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      cameraType: 'back',
-      mirrorMode: false,
-      imageUri: null,
+      imageList: [],
+      update: false,
     };
+  }
+  componentDidUpdate(){
+      if(this.state.update){
+        this.setState({
+          update:false
+        })
+      }
   }
   render() {
+    
+    console.log(this.state.imageList);
+    
     return (
       <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          captureAudio={false}
-          style={styles.preview}
-          type={this.state.cameraType}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          
-          // onGoogleVisionBarcodesDetected={({ barcodes }) => {
-          //   console.log(barcodes);
-          // }}
-        />
-        <View style={{flexDirection:'row',justifyContent:'center'}}>
-        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> SNAP </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.changeCamera.bind(this)} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> SWAP </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.capture} onPress={() => this.chooseFile('photo')}>
-          <Text style={styles.textStyle}>Choose Image</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=> this.props.navigation.navigate('save',{image:this.state.imageUri})} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> Save </Text>
-        </TouchableOpacity>
-        </View>
-        {this.state.imageUri && <Image source={{uri:this.state.imageUri}} style={{flex:1}}/>}
         
+          {this.state.imageList && 
+          <FlatList 
+            style={{maxHeight:600}}
+            numColumns={2}
+            horizontal={false}
+            data={this.state.imageList}
+            renderItem={({item}) => (
+              <View style={{flex:1,alignItems:'center'}}>
+            <Text></Text>
+            <Image style={{flex:1,height: 200, width:200,justifyContent:'center'}} source={{uri:item.path}}/>
+            </View>
+          )
+        }keyExtractor={(item, index) => index.toString()}
+        />
+      }
+        <View style={{flex:1,flexDirection:'row',justifyContent:'space-evenly'}}>
+
+          <TouchableOpacity onPress={this.imagePicker} style={styles.capture}>
+            <Text style={{color:'white'}}>Gallery</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.clickPicture} style={styles.capture}>
+            <Text style={{color:'white'}}>Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.capture}>
+            <Text style={{color:'white'}}>Submit</Text>
+          </TouchableOpacity>
+          </View>
       </View>
     );
+    
   }
-  changeCamera(){
-    if(this.state.cameraType==='back'){
-      this.setState({
-        cameraType: 'front'
-      })
+  clickPicture = async () =>
+  ImagePicker.openCamera({
+    compressImageQuality: 0.8,
+    cropping: true,
+  }).then(image => {
+    this.state.imageList.push(image)
+    this.setState({
+      update:true
+    })
+    // console.log(image);
+    console.log(this.state.imageList);
+  });
+imagePicker = async () => 
+  ImagePicker.openPicker({
+    multiple: true
+  }).then(images => {
+    for(let i=0;i<images.length;i++){
+      this.state.imageList.push(images[i])
     }
-    else{
-      this.setState({
-        cameraType: 'back'
-      })
-    }
-  }
-  takePicture = async () => {
-    if (this.camera) {
-      const options = { quality: 0.5, base64: true };
-      const data = await this.camera.takePictureAsync(options);
-      this.setState({
-        imageUri:data.uri
-      })
-      console.log(data.uri);
-    }
-  };
-  chooseFile = async (type) => {
-    let options = {
-      mediaType: type,
-      quality: 1,
-    };
-    launchImageLibrary(options, (response) => {
-      console.log('Response = ', response);
+    this.setState({
+      update:true
+    })
+    
+    console.log(this.state.imageList)
+  });
 
-      if (response.didCancel) {
-        alert('Image not picked');
-        return;
-      } else if (response.errorCode == 'camera_unavailable') {
-        alert('Camera not available on device');
-        return;
-      } else if (response.errorCode == 'permission') {
-        alert('Permission not satisfied');
-        return;
-      } else if (response.errorCode == 'others') {
-        alert(response.errorMessage);
-        return;
-      }
-      console.log('base64 -> ', response.assets[0].base64);
-      console.log('uri -> ', response.assets[0].uri);
-      console.log('width -> ', response.assets[0].width);
-      console.log('height -> ', response.assets[0].height);
-      console.log('fileSize -> ', response.assets[0].fileSize);
-      console.log('type -> ', response.assets[0].type);
-      console.log('fileName -> ', response.assets[0].fileName);
-      this.setState({
-        imageUri:response.assets[0].uri
-      })
-    });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    flexDirection: 'column'
   },
   preview: {
     flex: 1,
@@ -124,7 +97,7 @@ const styles = StyleSheet.create({
   },
   capture: {
     flex: 0,
-    backgroundColor: '#fff',
+    backgroundColor: '#694fad',
     borderRadius: 5,
     padding: 15,
     paddingHorizontal: 20,
