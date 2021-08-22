c9a16e19a6dbc0c616df748beacae550325580e2
Sairaj Mishra <rickysairaj@gmail.com>
2021-08-22 19:35:05 +0530

    Added bootsplash

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {View,Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import SplashScreen from  "react-native-splash-screen";
+
 const  Stack = createStackNavigator();
 
 
@@ -9,6 +11,9 @@ import Main from './components/Main.js';
 import Converter from './components/main/Converter.js';
 
 export default class App extends Component {
+  componentDidMount(){
+    SplashScreen.hide();
+  }
   render(){
     return (
       <View style={{flex:1}}>
@@ -20,6 +25,7 @@ export default class App extends Component {
             </Stack.Navigator>
           </NavigationContainer>  
       </View>
+      
 
     )
   }
